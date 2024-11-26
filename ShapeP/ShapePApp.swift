511103e866d2893ee9c0eef36b2d87b2682c67e4
Kevin Wu <KWu@lcs.on.ca>
2024-11-26 15:26:54 -0500

    Initial Commit

---
+++ ShapeP/ShapePApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapePApp.swift
+//  ShapeP
+//
+//  Created by Yixuan Wu on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct ShapePApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
