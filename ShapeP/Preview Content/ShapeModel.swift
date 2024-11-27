4cffa70c7bd34f912e0056f9b4fbbcf2cc98bf8e
Kevin Wu <KWu@lcs.on.ca>
2024-11-26 22:22:06 -0500

    Model setup

---
+++ ShapeP/Preview Content/ShapeModel.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeModel.swift
+//  ShapeP
+//
+//  Created by Yixuan Wu on 2024-11-26.
+//
+
+import SwiftUI
+
+struct ShapeModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapeModel()
+}
